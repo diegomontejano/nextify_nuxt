934714a901139ef1e69a649cb98990c5d2ba0af0
diegomontejano <diegomontejano@icloud.com>
2025-03-08 13:07:27 -0300

    first commit

---
+++ app/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="min-h-screen bg-gray-900 text-white flex items-center justify-center flex-col">
+
+    <h1 class="text-4xl font-bold">Nextify</h1>
+
+    <button @click="increment" class="mt-4 px-4 py-2 bg-blue-500 rounded hover:bg-blue-600">
+      Click here: {{ counter }}
+    </button>
+
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+function increment() {
+  counter.value++
+}
+</script>
